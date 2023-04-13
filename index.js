ebccc299c4fd69e33b82903f57f41665b90e5439
River Ferguson <riverferguson@gmail.com>
2023-04-12 20:44:04 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const pText = document.querySelector('#text')
+    pText.textContent = 'This is really cool!'
+    console.log("after")
+})
+
+console.log("before")
